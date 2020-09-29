c89dba8ca7eff6352316e27305d299c6649315ed
niejunlei <niejunlei@xueqiu.com>
2020-09-29 13:57:31 +0800

    init

---
+++ src/main/java/com/geetest/demo/FirstRegisterServlet.java
@@ -0,0 +1,44 @@
+package com.geetest.demo;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.geetest.sdk.GeetestLib;
+import com.geetest.sdk.GeetestLibResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+/**
+ * 一次验证接口，GET请求
+ */
+@CrossOrigin
+@WebServlet("/register")
+public class FirstRegisterServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        GeetestLib gtLib = new GeetestLib(GeetestConfig.GEETEST_ID, GeetestConfig.GEETEST_KEY);
+        String userId = "test";
+        String digestmod = "md5";
+        Map<String, String> paramMap = new HashMap<String, String>();
+        paramMap.put("digestmod", digestmod); // 必传参数，此版本sdk可支持md5、sha256、hmac-sha256，md5之外的算法需特殊配置的账号，联系极验客服
+        // 自定义参数,可选择添加
+        paramMap.put("user_id", userId); // 网站用户id
+        paramMap.put("client_type", "web"); // web:电脑上的浏览器；h5:手机上的浏览器，包括移动应用内完全内置的web_view；native：通过原生SDK植入APP应用的方式
+        paramMap.put("ip_address", "127.0.0.1"); // 传输用户请求验证时所携带的IP
+        GeetestLibResult result = gtLib.register(digestmod, paramMap);
+        // 将结果状态设置到session中
+        // 注意，此处api1接口存入session，api2会取出使用，格外注意session的存取和分布式环境下的应用场景
+        request.getSession().setAttribute(GeetestLib.GEETEST_SERVER_STATUS_SESSION_KEY, result.getStatus());
+        request.getSession().setAttribute("userId", userId);
+        // 注意，不要更改返回的结构和值类型
+        PrintWriter out = response.getWriter();
+        out.println(result.getData());
+    }
+}
---
+++ src/main/java/com/geetest/demo/SecondValidateServlet.java
@@ -0,0 +1,58 @@
+package com.geetest.demo;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.geetest.sdk.GeetestLib;
+import com.geetest.sdk.GeetestLibResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+/**
+ * 二次验证接口，POST请求
+ */
+@CrossOrigin
+@WebServlet("/validate")
+public class SecondValidateServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        GeetestLib gtLib = new GeetestLib(GeetestConfig.GEETEST_ID, GeetestConfig.GEETEST_KEY);
+        String challenge = request.getParameter(GeetestLib.GEETEST_CHALLENGE);
+        String validate = request.getParameter(GeetestLib.GEETEST_VALIDATE);
+        String seccode = request.getParameter(GeetestLib.GEETEST_SECCODE);
+        PrintWriter out = response.getWriter();
+        int status = 0;
+        String userId = "";
+        try {
+            // 从session中获取一次验证状态码和userId
+            status = (Integer) request.getSession().getAttribute(GeetestLib.GEETEST_SERVER_STATUS_SESSION_KEY);
+            userId = (String) request.getSession().getAttribute("userId");
+        } catch (Exception e) {
+            out.println("{\"result\":\"fail\",\"msg\":\"session取key发生异常\"}");
+            return;
+        }
+        GeetestLibResult result = null;
+        if (status == 1) {
+            Map<String, String> paramMap = new HashMap<String, String>();
+            // 自定义参数,可选择添加
+            paramMap.put("user_id", userId); // 网站用户id
+            paramMap.put("client_type", "web"); // web:电脑上的浏览器；h5:手机上的浏览器，包括移动应用内完全内置的web_view；native：通过原生SDK植入APP应用的方式
+            paramMap.put("ip_address", "127.0.0.1"); // 传输用户请求验证时所携带的IP
+            result = gtLib.successValidate(challenge, validate, seccode, paramMap);
+        } else {
+            result = gtLib.failValidate(challenge, validate, seccode);
+        }
+        if (result.getStatus() == 1) {
+            out.println(String.format("{\"result\":\"success\",\"version\":\"%s\"}", GeetestLib.VERSION));
+        } else {
+            out.println(String.format("{\"result\":\"fail\",\"version\":\"%s\",\"msg\":\"%s\"}", GeetestLib.VERSION, result.getMsg()));
+        }
+    }
+}
